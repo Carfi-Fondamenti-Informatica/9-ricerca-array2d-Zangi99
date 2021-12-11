def537a7cc5378c3d4914b97aef384619b00fabe
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-12-11 18:21:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,21 @@
 #include <iostream>
-#include <cstring>
+#include "lib.h"
 using namespace std;
 
-bool cerca(char lista[10][20], char nome[1][20], int &pos){
-  
-  for(int i=0; i<10; i++){
-    if(strcmp(lista[1], nome[0],)==0){
-      pos=i;
-      return true;}
-  }
-  return false;
-}
-
-   
-
-
 int main(){
-  int pos;
-  char A[10][20], B[1][20];
-  for (int i=0; i<10; i++){
-    cin>>A[i];
+  char lista[10][20];
+  char nome[20];
+  int a=0;
+  for (int i=0;i<10;i++){
+    cin>>lista[i];
   }
+  cin>>nome;
   
-  cin>>B[0];
-  
-  if(cerca(A, B, pos)){
-    cout<<pos<<endl;
+  if(res(list,nome,a)){
+    cout<<a;
+  }else{
+    cout<<"non presente";
   }
-  else{
-  cout<<"Non presente"<<endl;
-  }
-  
   return 0;
 }
+  
