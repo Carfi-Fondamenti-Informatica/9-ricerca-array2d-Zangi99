28953308bfe635022f997eca943fc034e7b989c8
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-12-11 13:15:32 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include "lib.h"
+#include <cstring>
+using namespace std;
+
+bool cerca(char lista[10][20], char nome[1][10], int &pos){
+    for (int i=0; i<10; i++){
+        if(strcmp(lista[i],nome[0])==0){
+            pos=i;
+            return true;
+        }
+    }return false;
+}
 
