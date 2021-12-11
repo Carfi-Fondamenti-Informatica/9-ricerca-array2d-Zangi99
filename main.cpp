edcdeb68f6f2ede108947b4f4f7311b86af341f9
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-12-11 13:23:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  int pos=0;
+  char A[10][20], B[1][20];
+  for (int i=0; i<10; i++){
+    cin>>a[i];
+  }
+  
+  cin>>B[0]
+  
+  if(cerca(A, B, pos)){
+    cout<<pos<<endl;
+  }else{
+    cout<<"Non presente"<<endl;
+       }
+  
   return 0;
 }
