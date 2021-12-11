8e7fd9ff41bd2b7aed88082e1f53bee84ac93bd6
Zangi99 <92801992+Zangi99@users.noreply.github.com>
2021-12-11 18:16:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,2 +1,12 @@
+#include "lib.h"
+#include <string.h>
 
+bool res(char lista[10][20], char nome[20], int &i){
+  for (i=0;i<10;i++){
+    if((strcmp(lista[i],nome))==0){
+      return true;
+    }
+  }
+  return false;
+}
 
